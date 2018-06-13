9a529907061caa9ed658c023be49ab9f0cc13262
thisdp <30386052+thisdp@users.noreply.github.com>
2018-06-13 12:03:47 +0800

    Update PropertyManager.lua

--- PropertyManager.lua
+++ PropertyManager.lua
@@ -9,152 +9,154 @@ function dgsSetData(element,key,value,nocheck)
 	if isElement(element) and dgsType then
 		dgsElementData[element] = dgsElementData[element] or {}
 		local oldValue = dgsElementData[element][key]
-		dgsElementData[element][key] = value
-		if not nocheck then
-			if dgsType == "dgs-dxscrollbar" then
-				if key == "length" then
-					local w,h = dgsGetSize(element,false)
-					local voh = dgsElementData[element].voh
-					if (value[2] and value[1]*(voh and w-h*2 or h-w*2) or value[1]) < 20 then
-						dgsElementData[element].length = {10,false}
-					end
-				elseif key == "position" then
-					if oldValue and oldValue ~= value then
-						triggerEvent("onDgsScrollBarScrollPositionChange",element,value,oldValue)
-					end
-				end
-			elseif (dgsType == "dgs-dxgridlist" or dgsType == "dgs-dxscrollpane") then
-				if key == "scrollBarThick" then
-					assert(type(value) == "number","Bad argument 'dgsSetData' at 3,expect number got"..type(value))
-					local scb = dgsElementData[element].scrollbars
-					local size = dgsElementData[element].absSize
-					dgsSetPosition(scb[1],size[1]-value,0,false)
-					dgsSetSize(scb[1],value,size[2]-value,false)
-					dgsSetPosition(scb[2],0,size[2]-value,false)
-					dgsSetSize(scb[2],size[1]-value,value,false)
-					if dgsType == "dgs-dxgridlist" then
-						configGridList(element)
-					else
-						configScrollPane(element)
-					end
-				elseif dgsType == "dgs-dxgridlist" then
-					if key == "columnHeight" or key == "mode" then
-						configGridList(element)
-					elseif key == "rowData" then
-						if dgsElementData[element].autoSort then
-							dgsElementData[element].nextRenderSort = true
+		if oldValue ~= value then
+			dgsElementData[element][key] = value
+			if not nocheck then
+				if dgsType == "dgs-dxscrollbar" then
+					if key == "length" then
+						local w,h = dgsGetSize(element,false)
+						local voh = dgsElementData[element].voh
+						if (value[2] and value[1]*(voh and w-h*2 or h-w*2) or value[1]) < 20 then
+							dgsElementData[element].length = {10,false}
+						end
+					elseif key == "position" then
+						if oldValue and oldValue ~= value then
+							triggerEvent("onDgsScrollBarScrollPositionChange",element,value,oldValue)
 						end
 					end
+				elseif (dgsType == "dgs-dxgridlist" or dgsType == "dgs-dxscrollpane") then
+					if key == "scrollBarThick" then
+						assert(type(value) == "number","Bad argument 'dgsSetData' at 3,expect number got"..type(value))
+						local scb = dgsElementData[element].scrollbars
+						local size = dgsElementData[element].absSize
+						dgsSetPosition(scb[1],size[1]-value,0,false)
+						dgsSetSize(scb[1],value,size[2]-value,false)
+						dgsSetPosition(scb[2],0,size[2]-value,false)
+						dgsSetSize(scb[2],size[1]-value,value,false)
+						if dgsType == "dgs-dxgridlist" then
+							configGridList(element)
+						else
+							configScrollPane(element)
+						end
+					elseif dgsType == "dgs-dxgridlist" then
+						if key == "columnHeight" or key == "mode" then
+							configGridList(element)
+						elseif key == "rowData" then
+							if dgsElementData[element].autoSort then
+								dgsElementData[element].nextRenderSort = true
+							end
+						end
+					end
+				elseif dgsType == "dgs-dxcombobox" then
+					if key == "scrollBarThick" then
+						assert(type(value) == "number","Bad argument 'dgsSetData' at 3,expect number got"..type(value))
+						local scrollbar = dgsElementData[element].scrollbar
+						configComboBox_Box(dgsElementData[element].myBox)
+					elseif key == "listState" then
+						triggerEvent("onDgsComboBoxStateChange",element,value == 1 and true or false)
+					end
+				elseif dgsType == "dgs-dxtabpanel" then
+					if key == "selected" then
+						local old,new = oldValue,value
+						local tabs = dgsElementData[element].tabs
+						triggerEvent("onDgsTabPanelTabSelect",element,new,old,tabs[new],tabs[old])
+					elseif key == "tabsidesize" then
+						local width = dgsElementData[element].absSize[1]
+						local change = value[2] and value[1]*width or value[1]
+						local old = oldValue[2] and oldValue[1]*width or oldValue[1]
+						local tabs = dgsElementData[element].tabs
+						dgsSetData(element,"allleng",dgsElementData[element].allleng+(change-old)*#tabs*2)
+					elseif key == "tabgapsize" then
+						local width = dgsElementData[element].absSize[1]
+						local change = value[2] and value[1]*width or value[1]
+						local old = oldValue[2] and oldValue[1]*width or oldValue[1]
+						local tabs = dgsElementData[element].tabs
+						dgsSetData(element,"allleng",dgsElementData[element].allleng+(change-old)*math.max((#tabs-1),1))
+					end
+				elseif dgsType == "dgs-dxtab" then
+					if key == "text" then
+						local absrltWidth = dgsElementData[element].absrltWidth
+						if absrltWidth[1] < 0 then
+							local tabpanel = dgsElementData[element].parent
+							local w = dgsElementData[tabpanel].absSize[1]
+							local t_minWid = dgsElementData[tabpanel].tabminwidth
+							local t_maxWid = dgsElementData[tabpanel].tabmaxwidth
+							local minwidth = t_minWid[2] and t_minWid[1]*w or t_minWid[1]
+							local maxwidth = t_maxWid[2] and t_maxWid[1]*w or t_maxWid[1]
+							dgsSetData(element,"width",math.restrict(minwidth,maxwidth,dxGetTextWidth(tostring(value),dgsElementData[element].textsize[1],dgsElementData[tabpanel].font)))
+						end
+					elseif key == "width" then
+						local absrltWidth = dgsElementData[element].absrltWidth
+						if absrltWidth[1] < 0 then
+							local tabpanel = dgsElementData[element].parent
+							dgsSetData(tabpanel,"allleng",dgsElementData[tabpanel].allleng+(value-oldValue))
+						end
+					elseif key == "absrltWidth" then
+					end
+				elseif dgsType == "dgs-dxedit" then
+					local gedit = dgsElementData[element].edit
+					if key == "maxLength" then
+						local value = tonumber(value)
+						if not value or not isElement(gedit) then return false end
+						return guiEditSetMaxLength(gedit,value)
+					elseif key == "readOnly" then
+						if not isElement(gedit) then return false end
+						return guiEditSetReadOnly(gedit,value and true or false)
+					elseif key == "text" then
+						dgsElementData[element].text = utf8.sub(value,0,dgsElementData[element].maxLength)
+						local txtSize = dgsElementData[element].textsize
+						dgsElementData[element].textFontLen = dxGetTextWidth(dgsElementData[element].text,txtSize[1],dgsElementData[element].font)
+					elseif key == "textsize" then
+						dgsElementData[element].textFontLen = dxGetTextWidth(dgsElementData[element].text,value[1],dgsElementData[element].font)
+					elseif key == "font" then
+						local txtSize = dgsElementData[element].textsize
+						dgsElementData[element].textFontLen = dxGetTextWidth(dgsElementData[element].text,txtSize[1],dgsElementData[element].font)
+					end
+				elseif dgsType == "dgs-dxmemo" then
+					if key == "readOnly" then
+						local gmemo = dgsElementData[element].memo
+						if not isElement(gmemo) then return false end
+						return guiMemoSetReadOnly(gmemo,value and true or false)
+					elseif key == "text" then
+						return handleDxMemoText(element,value)
+					end
+				elseif dgsType == "dgs-dxprogressbar" then
+					if key == "progress" then
+						triggerEvent("onDgsProgressBarChange",element,value,oldValue)
+					end
+				elseif dgsType == "dgs-dx3dinterface" then
+					if key == "size" then
+						local temprt = dgsElementData[element].renderTarget
+						if isElement(temprt) then
+							destroyElement(temprt)
+						end
+						local renderTarget = dxCreateRenderTarget(value[1],value[2],true)
+						dgsSetData(element,"renderTarget",renderTarget)
+					end
 				end
-			elseif dgsType == "dgs-dxcombobox" then
-				if key == "scrollBarThick" then
-					assert(type(value) == "number","Bad argument 'dgsSetData' at 3,expect number got"..type(value))
-					local scrollbar = dgsElementData[element].scrollbar
-					configComboBox_Box(dgsElementData[element].myBox)
-				elseif key == "listState" then
-					triggerEvent("onDgsComboBoxStateChange",element,value == 1 and true or false)
-				end
-			elseif dgsType == "dgs-dxtabpanel" then
-				if key == "selected" then
-					local old,new = oldValue,value
-					local tabs = dgsElementData[element].tabs
-					triggerEvent("onDgsTabPanelTabSelect",element,new,old,tabs[new],tabs[old])
-				elseif key == "tabsidesize" then
-					local width = dgsElementData[element].absSize[1]
-					local change = value[2] and value[1]*width or value[1]
-					local old = oldValue[2] and oldValue[1]*width or oldValue[1]
-					local tabs = dgsElementData[element].tabs
-					dgsSetData(element,"allleng",dgsElementData[element].allleng+(change-old)*#tabs*2)
-				elseif key == "tabgapsize" then
-					local width = dgsElementData[element].absSize[1]
-					local change = value[2] and value[1]*width or value[1]
-					local old = oldValue[2] and oldValue[1]*width or oldValue[1]
-					local tabs = dgsElementData[element].tabs
-					dgsSetData(element,"allleng",dgsElementData[element].allleng+(change-old)*math.max((#tabs-1),1))
-				end
-			elseif dgsType == "dgs-dxtab" then
 				if key == "text" then
-					local absrltWidth = dgsElementData[element].absrltWidth
-					if absrltWidth[1] < 0 then
-						local tabpanel = dgsElementData[element].parent
-						local w = dgsElementData[tabpanel].absSize[1]
-						local t_minWid = dgsElementData[tabpanel].tabminwidth
-						local t_maxWid = dgsElementData[tabpanel].tabmaxwidth
-						local minwidth = t_minWid[2] and t_minWid[1]*w or t_minWid[1]
-						local maxwidth = t_maxWid[2] and t_maxWid[1]*w or t_maxWid[1]
-						dgsSetData(element,"width",math.restrict(minwidth,maxwidth,dxGetTextWidth(tostring(value),dgsElementData[element].textsize[1],dgsElementData[tabpanel].font)))
-					end
-				elseif key == "width" then
-					local absrltWidth = dgsElementData[element].absrltWidth
-					if absrltWidth[1] < 0 then
-						local tabpanel = dgsElementData[element].parent
-						dgsSetData(tabpanel,"allleng",dgsElementData[tabpanel].allleng+(value-oldValue))
-					end
-				elseif key == "absrltWidth" then
-				end
-			elseif dgsType == "dgs-dxedit" then
-				local gedit = dgsElementData[element].edit
-				if key == "maxLength" then
-					local value = tonumber(value)
-					if not value or not isElement(gedit) then return false end
-					return guiEditSetMaxLength(gedit,value)
-				elseif key == "readOnly" then
-					if not isElement(gedit) then return false end
-					return guiEditSetReadOnly(gedit,value and true or false)
-				elseif key == "text" then
-					dgsElementData[element].text = utf8.sub(value,0,dgsElementData[element].maxLength)
-					local txtSize = dgsElementData[element].textsize
-					dgsElementData[element].textFontLen = dxGetTextWidth(dgsElementData[element].text,txtSize[1],dgsElementData[element].font)
-				elseif key == "textsize" then
-					dgsElementData[element].textFontLen = dxGetTextWidth(dgsElementData[element].text,value[1],dgsElementData[element].font)
-				elseif key == "font" then
-					local txtSize = dgsElementData[element].textsize
-					dgsElementData[element].textFontLen = dxGetTextWidth(dgsElementData[element].text,txtSize[1],dgsElementData[element].font)
-				end
-			elseif dgsType == "dgs-dxmemo" then
-				if key == "readOnly" then
-					local gmemo = dgsElementData[element].memo
-					if not isElement(gmemo) then return false end
-					return guiMemoSetReadOnly(gmemo,value and true or false)
-				elseif key == "text" then
-					return handleDxMemoText(element,value)
-				end
-			elseif dgsType == "dgs-dxprogressbar" then
-				if key == "progress" then
-					triggerEvent("onDgsProgressBarChange",element,value,oldValue)
-				end
-			elseif dgsType == "dgs-dx3dinterface" then
-				if key == "size" then
-					local temprt = dgsElementData[element].renderTarget
-					if isElement(temprt) then
-						destroyElement(temprt)
-					end
-					local renderTarget = dxCreateRenderTarget(value[1],value[2],true)
-					dgsSetData(element,"renderTarget",renderTarget)
-				end
-			end
-			if key == "text" then
-				triggerEvent("onDgsTextChange",element)
-			elseif key == "visible" and not value then
-				for k,v in ipairs(getElementsByType("dgs-dxedit")) do
-					local parent = v
-					for i=1,500 do
-						parent = dgsElementType[parent] == "dgs-dxtab" and dgsElementData[parent].parent or FatherTable[parent]
-						if not parent then break end
-						if parent == element then
-							guiSetVisible(dgsElementData[v].edit,false)
-							break
+					triggerEvent("onDgsTextChange",element)
+				elseif key == "visible" and not value then
+					for k,v in ipairs(getElementsByType("dgs-dxedit")) do
+						local parent = v
+						for i=1,500 do
+							parent = dgsElementType[parent] == "dgs-dxtab" and dgsElementData[parent].parent or FatherTable[parent]
+							if not parent then break end
+							if parent == element then
+								guiSetVisible(dgsElementData[v].edit,false)
+								break
+							end
 						end
 					end
-				end
-				for k,v in ipairs(getElementsByType("dgs-dxmemo")) do
-					local parent = v
-					for i=1,500 do
-						parent = dgsElementType[parent] == "dgs-dxtab" and dgsElementData[parent].parent or FatherTable[parent]
-						if not parent then break end
-						if parent == element then
-							guiSetVisible(dgsElementData[v].memo,false)
-							break
+					for k,v in ipairs(getElementsByType("dgs-dxmemo")) do
+						local parent = v
+						for i=1,500 do
+							parent = dgsElementType[parent] == "dgs-dxtab" and dgsElementData[parent].parent or FatherTable[parent]
+							if not parent then break end
+							if parent == element then
+								guiSetVisible(dgsElementData[v].memo,false)
+								break
+							end
 						end
 					end
 				end
