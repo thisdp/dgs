b63c89bbc487db8e8d498eee1a88fb861c2cc381
thisdp <30386052+thisdp@users.noreply.github.com>
2018-06-20 08:14:34 +0800

    optimize

--- Core/scrollpane.lua
+++ Core/scrollpane.lua
@@ -36,56 +36,60 @@ function dgsCreateScrollPane(x,y,sx,sy,relative,parent)
 	dgsSetData(scrollbar1,"scrollType","Vertical")
 	dgsSetData(scrollbar2,"scrollType","Horizontal")
 	triggerEvent("onDgsCreate",scrollpane)
+	if not isElement(renderTarget) then
+		destroyElement(scrollpane)
+		return false
+	end
 	return scrollpane
 end
 
 addEventHandler("onDgsCreate",root,function()
 	local parent = dgsGetParent(source)
 	if isElement(parent) and dgsGetType(parent) == "dgs-dxscrollpane" then
-		local relativePos,relativeSize = unpack(dgsGetData(source,"relative"))
+		local relativePos,relativeSize = dgsElementData[source].relative[1],dgsElementData[source].relative[2]
 		local x,y,sx,sy
 		if relativePos then
-			x,y = unpack(dgsGetData(source,"rltPos"))
+			x,y = dgsElementData[source].rltPos[1],dgsElementData[source].rltPos[2]
 		end
 		if relativeSize then
-			sx,sy = unpack(dgsGetData(source,"rltSize"))
+			sx,sy = dgsElementData[source].rltSize[1],dgsElementData[source].rltSize[2]
 		end
 		calculateGuiPositionSize(source,x,y,relativePos or _,sx,sy,relativeSize or _)
-		local sx,sy = unpack(dgsGetData(source,"absSize"))
-		local x,y = unpack(dgsGetData(source,"absPos"))
-		local maxSize = dgsGetData(parent,"maxChildSize")
+		local sx,sy = dgsElementData[source].absSize[1],dgsElementData[source].absSize[2]
+		local x,y = dgsElementData[source].absPos[1],dgsElementData[source].absPos[2]
+		local maxSize = dgsElementData[parent].maxChildSize
 		local tempx,tempy = x+sx,y+sy
 		local ntempx,ntempy
 		if maxSize[1] <= tempx then
 			ntempx = 0
 			for k,v in ipairs(dgsGetChildren(parent)) do
-				local pos = dgsGetData(source,"absPos")
-				local size = dgsGetData(source,"absSize")
+				local pos = dgsElementData[source].absPos
+				local size = dgsElementData[source].absSize
 				ntempx = ntempx > pos[1]+size[1] and ntempx or pos[1]+size[1]
 			end
 		end
 		if maxSize[2] <= tempy then
 			ntempy = 0
 			for k,v in ipairs(dgsGetChildren(parent)) do
-				local pos = dgsGetData(source,"absPos")
-				local size = dgsGetData(source,"absSize")
+				local pos = dgsElementData[source].absPos
+				local size = dgsElementData[source].absSize
 				ntempy = ntempy > pos[2]+size[2] and ntempy or pos[2]+size[2]	
 			end
 		end
 		dgsSetData(parent,"maxChildSize",{ntempx or maxSize[1],ntempy or maxSize[2]})
 		dgsScrollPaneUpdateScrollBar(parent,ntempx or maxSize[1],ntempy or maxSize[2])
-		local renderTarget = dgsGetData(parent,"renderTarget_parent")
+		local renderTarget = dgsElementData[parent].renderTarget_parent
 		if isElement(renderTarget) then
 			destroyElement(renderTarget)
 		end
-		local scrbThick = dgsGetData(parent,"scrollBarThick")
-		local scrollbar = dgsGetData(parent,"scrollbars")
+		local scrbThick = dgsElementData[parent].scrollBarThick
+		local scrollbar = dgsElementData[parent].scrollbars
 		local xthick,ythick = 0,0
 		if scrollbar then
 			xthick = dgsGetVisible(scrollbar[1]) and scrbThick or 0
 			ythick = dgsGetVisible(scrollbar[2]) and scrbThick or 0
 		end
-		local sx,sy = unpack(dgsGetData(parent,"absSize"))
+		local sx,sy = dgsElementData[parent].absSize[1],dgsElementData[parent].absSize[2]
 		local renderTarget = dxCreateRenderTarget(sx-xthick,sy-ythick,true)
 		dgsSetData(parent,"renderTarget_parent",renderTarget)
 	end
@@ -95,11 +99,11 @@ function dgsScrollPaneUpdateScrollBar(scrollpane,ntempx,ntempy)
 	if dgsGetType(scrollpane) == "dgs-dxscrollpane" then
 		local scrollbar = dgsGetData(scrollpane,"scrollbars")
 		if isElement(scrollbar[1]) and isElement(scrollbar[2]) then
-			local ntmpx,ntmpy = unpack(dgsGetData(scrollpane,"maxChildSize"))
+			local ntmpx,ntmpy = dgsElementData[scrollpane].maxChildSize[1],dgsElementData[scrollpane].maxChildSize[2]
 			ntempx,ntempy = ntempx or ntmpx,ntempy or ntmpy
-			local sx,sy = unpack(dgsGetData(scrollpane,"absSize"))
+			local sx,sy = dgsElementData[scrollpane].absSize[1],dgsElementData[scrollpane].absSize[2]
 			local scbstate = {dgsGetVisible(scrollbar[1]),dgsGetVisible(scrollbar[2])}
-			local scbThick = dgsGetData(scrollpane,"scrollBarThick")
+			local scbThick = dgsElementData[scrollpane].scrollBarThick
 			local xthick,ythick = scbstate[1] and scbThick or 0,scbstate[2] and scbThick or 0
 			
 			if ntempx then
@@ -124,17 +128,17 @@ addEventHandler("onDgsDestroy",root,function()
 	local parent = dgsGetParent(source)
 	if isElement(parent) then
 		if dgsGetType(parent) == "dgs-dxscrollpane" then
-			local x,y = unpack(dgsGetData(source,"absPos"))
-			local sx,sy = unpack(dgsGetData(source,"absSize"))
-			local maxSize = dgsGetData(parent,"maxChildSize")
+			local sx,sy = dgsElementData[source].absSize[1],dgsElementData[source].absSize[2]
+			local x,y = dgsElementData[source].absPos[1],dgsElementData[source].absPos[2]
+			local maxSize = dgsElementData[parent].maxChildSize
 			local tempx,tempy = x+sx,y+sy
 			local ntempx,ntempy
 			if maxSize[1]-10 <= tempx then
 				ntempx = 0
 				for k,v in ipairs(dgsGetChildren(parent)) do
 					if v ~= source then
-						local pos = dgsGetData(v,"absPos")
-						local size = dgsGetData(v,"absSize")
+						local pos = dgsElementData[v].absPos
+						local size = dgsElementData[v].absSize
 						ntempx = ntempx > pos[1]+size[1] and ntempx or pos[1]+size[1]
 					end
 				end
@@ -143,8 +147,8 @@ addEventHandler("onDgsDestroy",root,function()
 				ntempy = 0
 				for k,v in ipairs(dgsGetChildren(parent)) do
 					if v ~= source then
-						local pos = dgsGetData(v,"absPos")
-						local size = dgsGetData(v,"absSize")
+						local pos = dgsElementData[v].absPos
+						local size = dgsElementData[v].absSize
 						ntempy = ntempy > pos[2]+size[2] and ntempy or pos[2]+size[2]
 					end
 				end	
@@ -156,14 +160,14 @@ addEventHandler("onDgsDestroy",root,function()
 end)
 
 function configScrollPane(source)
-	local renderTarget = dgsGetData(source,"renderTarget_parent")
+	local renderTarget = dgsElementData[source].renderTarget_parent
 	if isElement(renderTarget) then
 		destroyElement(renderTarget)
 	end
-	local sx,sy = unpack(dgsGetData(source,"absSize"))
-	local x,y = unpack(dgsGetData(source,"absPos"))
-	local scrbThick = dgsGetData(source,"scrollBarThick")
-	local scrollbar = dgsGetData(source,"scrollbars")
+	local sx,sy = dgsElementData[source].absSize[1],dgsElementData[source].absSize[2]
+	local x,y = dgsElementData[source].absPos[1],dgsElementData[source].absPos[2]
+	local scrbThick = dgsElementData[source].scrollBarThick
+	local scrollbar = dgsElementData[source].scrollbars
 	local xthick,ythick = 0,0
 	dgsScrollPaneUpdateScrollBar(source)
 	if scrollbar then
@@ -171,7 +175,7 @@ function configScrollPane(source)
 		local titleOffset = 0
 		if isElement(parent) then
 			if not dgsElementData[source].withoutTitle then
-				titleOffset = dgsGetData(parent,"titlesize") or 0
+				titleOffset = dgsElementData[parent].titlesize or 0
 			end
 		end
 		xthick = dgsGetVisible(scrollbar[1]) and scrbThick or 0
@@ -186,9 +190,9 @@ function configScrollPane(source)
 end
 
 function sortScrollPane(source,parent)
-	local sx,sy = unpack(dgsGetData(source,"absSize"))
-	local x,y = unpack(dgsGetData(source,"absPos"))
-	local maxSize = dgsGetData(parent,"maxChildSize")
+	local sx,sy = dgsElementData[source].absSize[1],dgsElementData[source].absSize[2]
+	local x,y = dgsElementData[source].absPos[1],dgsElementData[source].absPos[2]
+	local maxSize = dgsElementData[parent].maxChildSize
 	local tempx,tempy = x+sx,y+sy
 	local ntempx,ntempy
 	if maxSize[1] <= tempx then
@@ -196,8 +200,8 @@ function sortScrollPane(source,parent)
 	else
 		ntempx = 0
 		for k,v in ipairs(dgsGetChildren(parent)) do
-			local pos = dgsGetData(v,"absPos")
-			local size = dgsGetData(v,"absSize")
+			local pos = dgsElementData[v].absPos
+			local size = dgsElementData[v].absSize
 			ntempx = ntempx > pos[1]+size[1] and ntempx or pos[1]+size[1]
 		end
 	end
@@ -206,8 +210,8 @@ function sortScrollPane(source,parent)
 	else
 		ntempy = 0
 		for k,v in ipairs(dgsGetChildren(parent)) do
-			local pos = dgsGetData(v,"absPos")
-			local size = dgsGetData(v,"absSize")
+			local pos = dgsElementData[v].absPos
+			local size = dgsElementData[v].absSize
 			ntempy = ntempy > pos[2]+size[2] and ntempy or pos[2]+size[2]
 		end
 	end
