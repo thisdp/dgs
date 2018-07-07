a07a93efaac1b29988f59878cec42d0d7c87c629
thisdp <30386052+thisdp@users.noreply.github.com>
2018-07-07 16:17:27 +0800

    Update dgsExportedFunction.lua

--- dgsExportedFunction.lua
+++ dgsExportedFunction.lua
@@ -21,7 +21,7 @@ if not dgsImportHead then
 	function DGSCallMT:__index(k)
 		if type(k) ~= 'string' then k = tostring(k) end
 		self[k] = function(...)
-			assert(dgsImportHead,"DGS is not running")
+			assert(dgsImportHead,"DGS import data is missing or DGS is not running, please reimport dgs functions")
 			if type(dgsImportHead.dgsResource) == 'userdata' and getResourceRootElement(dgsImportHead.dgsResource) then
 				return call(dgsImportHead.dgsResource, k, ...)
 			else
