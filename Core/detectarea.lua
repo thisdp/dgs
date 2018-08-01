54f08771453416f906408aaf1957f8573e9c57a6
thisdp <30386052+thisdp@users.noreply.github.com>
2018-08-01 12:05:16 +0800

    Add files via upload

--- Core/detectarea.lua
+++ Core/detectarea.lua
@@ -1,17 +1,39 @@
-function dgsCreateDetectArea(x,y,sx,sy,strfnc,relative,parent,color)
+function dgsCreateDetectArea(x,y,sx,sy,relative,parent,color)
 	assert(tonumber(x),"Bad argument @dgsCreateDetectArea at argument 1, expect number got "..type(x))
 	assert(tonumber(y),"Bad argument @dgsCreateDetectArea at argument 2, expect number got "..type(y))
 	assert(tonumber(sx),"Bad argument @dgsCreateDetectArea at argument 3, expect number got "..type(sx))
 	assert(tonumber(sy),"Bad argument @dgsCreateDetectArea at argument 4, expect number got "..type(sy))
-	assert(tonumber(strfnc),"Bad argument @dgsCreateDetectArea at argument 5, expect string got "..type(strfnc))
 	if isElement(parent) then
 		assert(dgsIsDxElement(parent),"Bad argument @dgsCreateDetectArea at argument 7, expect dgs-dxgui got "..dgsGetType(parent))
 	end
 	local detectarea = createElement("dgs-dxdetectarea")
 	dgsSetType(detectarea,"dgs-dxdetectarea")
-	local _x = dgsIsDxElement(parent) and dgsSetParent(detectarea,parent,true) or table.insert(CenterFatherTable,1,detectarea)
+	dgsSetData(detectarea,"checkFunction",dgsDetectAreaDefaultFunction)
+	if isElement(parent) then
+		dgsSetParent(detectarea,parent)
+	else
+		table.insert(CenterFatherTable,detectarea)
+	end
 	insertResourceDxGUI(sourceResource,detectarea)
+	triggerEvent("onDgsPreCreate",detectarea)
 	calculateGuiPositionSize(detectarea,x,y,relative or false,sx,sy,relative or false,true)
 	triggerEvent("onDgsCreate",detectarea)
 	return detectarea
+end
+
+detectAreaPreDefine = [[
+	local args = {...}
+	local mxRlt,myRlt,mxAbs,myAbs = args[1],args[2],args[3],args[4]
+]]
+
+function dgsDetectAreaDefaultFunction(mxRlt,myRlt,mxAbs,myAbs)
+	return true
+end
+
+function dgsDetectAreaSetFunction(detectarea,fncStr)
+	assert(dgsGetType(detectarea) == "dgs-dxdetectarea","Bad argument @dgsDetectAreaSetFunction at argument 1, except dgs-dxdetectarea got "..dgsGetType(detectarea))
+	assert(type(fncStr) == "string","Bad argument @dgsDetectAreaSetFunction at argument 2, expect string got "..type(fncStr))
+	local fnc = loadstring(detectAreaPreDefine..fncStr)
+	assert(type(fnc) == "function","Bad argument @dgsDetectAreaSetFunction at argument 2, failed to load function")
+	return dgsSetData(detectarea,"checkFunction",fnc)
 end
