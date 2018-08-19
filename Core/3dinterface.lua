2ccb0bab26ced8b7698e60dd0180f6c5d65b7372
thisdp <30386052+thisdp@users.noreply.github.com>
2018-08-19 23:05:54 +0800

    3d interface fix
    idk

--- Core/3dinterface.lua
+++ Core/3dinterface.lua
@@ -30,7 +30,7 @@ function dgsCreate3DInterface(x,y,z,w,h,resolX,resolY,color,faceX,faceY,faceZ,di
 	end
 	return interface
 end
---[[
+
 function dgsDrawMaterialLine3D(x,y,z,vx,vy,vz,material,w,h,color,lnVec,lnPnt)
 	local offFaceX = atan2(vz,(vx^2+vy^2)^0.5)
 	local offFaceZ = atan2(vx,vy)
@@ -49,32 +49,6 @@ function dgsDrawMaterialLine3D(x,y,z,vx,vy,vz,material,w,h,color,lnVec,lnPnt)
 		local vec1X,vec1Y,vec1Z = ltX+x-px,ltY+y-py,ltZ+z-pz
 		local vec2X,vec2Y,vec2Z = px-x+x1,py-y+y1,pz-z+z1
 		local _x,_y = (vec1X*ltX+vec1Y*ltY+vec1Z*ltZ)/(ltX^2+ltY^2+ltZ^2)^0.5/w,(vec2X*x1+vec2Y*y1+vec2Z*z1)/(x1^2+y1^2+z1^2)^0.5/_h
-		local angle = (x-lnPnt[1])*lnVec[1]+(y-lnPnt[2])*lnVec[2]+(z-lnPnt[3])*lnVec[3]
-		local inSide = _x>=0 and _x<=1 and _y>=0 and _y <=1
-		return (angle > 0) and inSide,_x,_y,px,py,pz
-	end
-end]]
-
-function dgsDrawMaterialLine3D(x,y,z,vx,vy,vz,material,w,h,color,lnVec,lnPnt)
-	local offFaceX = atan2(vz,(vx^2+vy^2)^0.5)
-	local offFaceZ = atan2(vx,vy)
-	local _h=h
-	h=h/2
-	local x1,y1,z1 = sin(offFaceX)*sin(offFaceZ)*h,sin(offFaceX)*cos(offFaceZ)*h,-cos(offFaceX)*h
-	dxDrawMaterialLine3D(x-x1,y-y1,z-z1,x+x1,y+y1,z+z1,material,w,tocolor(255,255,255,255),x+vx,y+vy,z+vz)
-	if lnVec and lnPnt then
-		local px,py,pz = dgsGetIntersection(lnVec,lnPnt,{vx,vy,vz},{x,y,z}) --Intersection Point
-		if not px then return end
-		local model = (vx*vx+vy*vy+vz*vz)^0.5
-		local vx,vy,vz = vx/model,vy/model,vz/model
-		local ltX,ltY,ltZ = y1*vz-vy*z1,z1*vx-vz*x1,x1*vy-vx*y1 --Left Point
-		local ltX2,ltY2,ltZ2 = ltX*ltX,ltY*ltY,ltZ*ltZ
-		local leftModel = (ltX*ltX+ltY*ltY+ltZ*ltZ)^0.5
-		local leftModel2 = leftModel*2/w
-		local ltX,ltY,ltZ = ltX/leftModel2,ltY/leftModel2,ltZ/leftModel2
-		local vec1X,vec1Y,vec1Z = ltX+x-px,ltY+y-py,ltZ+z-pz
-		local vec2X,vec2Y,vec2Z = px-x+x1,py-y+y1,pz-z+z1
-		local _x,_y = (vec1X*ltX+vec1Y*ltY+vec1Z*ltZ)/leftModel/w,(vec2X*x1+vec2Y*y1+vec2Z*z1)/(x1^2+y1^2+z1^2)^0.5/_h
 		local angle = (x-lnPnt[1])*lnVec[1]+(y-lnPnt[2])*lnVec[2]+(z-lnPnt[3])*lnVec[3]
 		local inSide = _x>=0 and _x<=1 and _y>=0 and _y <=1
 		return (angle > 0) and inSide,_x,_y,px,py,pz
