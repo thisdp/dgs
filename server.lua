77964eb04a26ea5dc3de9034885fc68df182a606
thisdp <thisdp@gmail.com>
2020-03-06 20:07:26 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,15 @@
 ﻿addEvent("DGSI_RequestQRCode",true)
 addEvent("DGSI_RequestIP",true)
+addEvent("DGSI_RequestRemoteImage",true)
 addEventHandler("DGSI_RequestQRCode",root,function(str,w,h,id)
 	fetchRemote("https://api.qrserver.com/v1/create-qr-code/?size="..w.."x"..h.."&data="..str,{},function(data,info,player,id)
 		triggerClientEvent(player,"DGSI_ReceiveQRCode",player,data,info.success,id)
+	end,{client,id})
+end)
+
+addEventHandler("DGSI_RequestRemoteImage",root,function(website,id)
+	fetchRemote(website,{},function(data,info,player,id)
+		triggerClientEvent(player,"DGSI_ReceiveRemoteImage",player,data,info,id)
 	end,{client,id})
 end)
 
