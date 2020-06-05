4c1f76e6718fc354bcbf0ff6fc695119132917d2
Abasalt-yar <AbasaltYarmohammadzey@Gmail.Com>
2020-06-05 15:27:33 +0430

    File Loader Adds Files into meta

---
+++ fileLoader.lua
@@ -0,0 +1,64 @@
+-- Style Table => StyleName (Folder) => FilePath
+
+Styles = {
+	["myStyle"] = {
+		"styleSettings.txt",
+	}
+}
+
+local meta = XML.load("meta.xml",false)
+
+function isFileInMeta (filePath)
+	for _,node in ipairs(meta:getChildren()) do 
+		if node:getName() == "file" then 
+			if node:getAttribute("src") == filePath then 
+				return true
+			end
+		end
+	end
+	return false
+end
+
+function insertFileToMeta (path) 
+	local fileTag = meta:createChild("file")
+	fileTag:setAttribute("src",path)
+	outputDebugString("File "..path.." Added To Meta !")
+	
+end
+
+addEventHandler("onResourceStart",resourceRoot,function ()
+	local tick = getTickCount()
+	local restartRequired = false
+	local count = 0
+
+	for i,s in pairs(Styles) do 
+		for _,v in pairs(s) do 
+			local path = "StyleManager/"..tostring(i).."/"..v
+			if not isFileInMeta(path) then 
+				if not File.exists(path) then outputDebugString("File "..path.." Doesn't Exist !",1,255,0,0) else
+					insertFileToMeta(path)
+					restartRequired = true
+					count = count + 1
+				end
+			end
+		end
+	end
+
+	if restartRequired == true then 
+		local final = getTickCount() - tick
+		outputDebugString("Required Restarting "..getResourceName(getThisResource()).." | Restarting In 5 Seconds | Adedd Totally "..count.." File(s) In "..final.." Mili-Second(s)")
+		setTimer(restartResource,5000,1)
+	end
+end)
+
+local _restartResource = restartResource
+
+function restartResource ()
+	assert(hasObjectPermissionTo(getThisResource(),"function.restartResource"),"Missing ACL permission : function.restartResource")
+	_restartResource(getThisResource())
+end
+
+addEventHandler("onResourceStop",resourceRoot,function ()
+	meta:saveFile()
+	meta:unload()
+end)
