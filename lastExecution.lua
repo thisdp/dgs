a52cee5ebb006d0b8d1f5323226c44584d0536d0
thisdp <30386052+thisdp@users.noreply.github.com>
2021-03-06 13:35:26 +0800

    move

--- lastExecution.lua
+++ lastExecution.lua
@@ -1,1 +1,26 @@
 _G.loadstring = nil
+
+------------------Security
+DGSFileVerify = false
+addEventHandler("onDgsStart",resourceRoot,function()
+	triggerServerEvent("DGSI_RequestFileInfo",localPlayer)
+end)
+
+function verifyFiles()
+	local mismatched = {}
+	for fName,fData in pairs(DGSFileVerify) do
+		local fileInfo = {hashFile(fName)}
+		if fileInfo[1] ~= fData[1] or fileInfo[2] ~= fData[2] then
+			mismatched[fName] = fData
+		end
+	end
+	if table.count(mismatched) > 0 then
+		triggerServerEvent("DGSI_AbnormalDetected",localPlayer,mismatched)
+	end
+end
+
+addEvent("DGSI_ReceiveFileInfo",true)
+addEventHandler("DGSI_ReceiveFileInfo",root,function(data)
+	DGSFileVerify = data
+	verifyFiles()
+end)
