991896121b9987a1e562ba4277ef0985069d8919
thisdp <30386052+thisdp@users.noreply.github.com>
2021-04-08 20:57:55 +0800

    Update lastExecution.lua

--- lastExecution.lua
+++ lastExecution.lua
@@ -7,9 +7,11 @@ end)
 function verifyFiles()
 	local mismatched = {}
 	for fName,fData in pairs(DGSFileVerify) do
-		local fileInfo = {hashFile(fName)}
-		if fileInfo[1] ~= fData[1] or fileInfo[2] ~= fData[2] then
-			mismatched[fName] = fData
+		if fileExists(fName) then	--Skip the file that doesn't exist (maybe cache=false)
+			local fileInfo = {hashFile(fName)}
+			if fileInfo[1] ~= fData[1] or fileInfo[2] ~= fData[2] then
+				mismatched[fName] = fData
+			end
 		end
 	end
 	if table.count(mismatched) > 0 then
