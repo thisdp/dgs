608ae2a8168f9ddba5b8fdd479ea3e3b4b26b3e1
Nawaf Alqarni <30420446+xLive@users.noreply.github.com>
2021-08-14 03:27:50 +0300

    Fix an error when using dgsSetCustomCursorImage
    'styleSettings' (a nil value)

--- Core/customCursor.lua
+++ Core/customCursor.lua
@@ -5,7 +5,10 @@ CursorData = {
 }
 
 function dgsSetCustomCursorImage(cursorType,image,rotation,rotationCenter,offset,scale)
-	local style = styleSettings.cursor
+	local res = sourceResource or "global"
+	local style = styleManager.styles[res]
+	local using = style.using
+	style = style.loaded[using].cursor
 	if not cursorType then	--Apply all (image in current style)
 		for k,v in pairs(cursorTypesBuiltIn) do
 			local texture = dgsCreateTextureFromStyle(_,_style[k].image)
