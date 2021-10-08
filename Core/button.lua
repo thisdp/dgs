thisdp/dgs

commit a4e4884c471489b2f229d6fc9ce34344b8f73a27

fix

Core/button.lua
@@ -73,7 +73,7 @@ function dgsCreateButton(...)
 		clickOffset = {0,0},
 		clickType = 1;	--1:LMB;2:Wheel;3:RM,
 		clip = false,
-		colorTransformPeriod = 0, --ms
+		colorTransitionPeriod = 0, --ms
 		color = {normalColor, hoveringColor, clickedColor},
 		colorcoded = false,
 		font = style.font or systemFont,
@@ -151,8 +151,8 @@ dgsRenderer["dgs-dxbutton"] = function(source,x,y,w,h,mx,my,cx,cy,enabledInherit
 			finalcolor = tocolor(average,average,average,a*parentAlpha)
 		else
 			local targetColor = colors[buttonState]
-			if eleData.colorTransformPeriod > 0 then
-				renderBuffer.currentColor = interpolateColor(renderBuffer.startColor or targetColor,targetColor,(getTickCount()-eleData.currentStateTick)/eleData.colorTransformPeriod) -- todo
+			if eleData.colorTransitionPeriod > 0 then
+				renderBuffer.currentColor = interpolateColor(renderBuffer.startColor or targetColor,targetColor,(getTickCount()-eleData.currentStateTick)/eleData.colorTransitionPeriod) -- todo
 				finalcolor = renderBuffer.currentColor
 			else
 				finalcolor = targetColor
@@ -160,8 +160,8 @@ dgsRenderer["dgs-dxbutton"] = function(source,x,y,w,h,mx,my,cx,cy,enabledInherit
 		end
 	else
 		local targetColor = colors[buttonState]
-		if eleData.colorTransformPeriod > 0 and getTickCount()-eleData.currentStateTick <= eleData.colorTransformPeriod then
-			renderBuffer.currentColor = interpolateColor(renderBuffer.startColor or targetColor,targetColor,(getTickCount()-eleData.currentStateTick)/eleData.colorTransformPeriod) -- todo
+		if eleData.colorTransitionPeriod > 0 and getTickCount()-eleData.currentStateTick <= eleData.colorTransitionPeriod then
+			renderBuffer.currentColor = interpolateColor(renderBuffer.startColor or targetColor,targetColor,(getTickCount()-eleData.currentStateTick)/eleData.colorTransitionPeriod) -- todo
 			finalcolor = renderBuffer.currentColor
 		else
 			finalcolor = targetColor

plugin/svg.lua
@@ -26,11 +26,14 @@ dgsCustomTexture["dgs-dxsvg"] = function(posX,posY,width,height,u,v,usize,vsize,
 		svgSetDocumentXML(image,eleData.svgDocument)
 		eleData.svgDocumentUpdate = false
 	end
+	dxSetBlendMode("add")
 	if u and v and usize and vsize then
-		return dxDrawImageSection(posX,posY,width,height,u,v,usize,vsize,image,rotation,rotationX,rotationY,color,postGUI)
+		dxDrawImageSection(posX,posY,width,height,u,v,usize,vsize,image,rotation,rotationX,rotationY,color,postGUI)
 	else
-		return dxDrawImage(posX,posY,width,height,image,rotation,rotationX,rotationY,color,postGUI)
+		dxDrawImage(posX,posY,width,height,image,rotation,rotationX,rotationY,color,postGUI)
 	end
+	dxSetBlendMode("blend")
+	return true
 end
 
 function dgsSVGGetSize(svg)
@@ -74,14 +77,15 @@ function dgsSVGSetElementStyle(element,styleTable)
 	return xmlNodeSetAttribute(element,"style",toStyle(styleTable))
 end
 
-function dgsSVGCreateRect(svg,x,y,width,height,rx,ry)
+function dgsSVGCreateRect(svg,x,y,width,height,parent,rx,ry)
 	if not(dgsIsType(svg,"svg")) then error(dgsGenAsrt(svg,"dgsSVGCreateRect",1,"svg")) end
-	local newRect = xmlCreateChild(dgsElementData[svg].svgDocument,"rect")
+	local newRect = xmlCreateChild(parent or dgsElementData[svg].svgDocument,"rect")
 	xmlNodeSetAttribute(newRect,"x",x)
 	xmlNodeSetAttribute(newRect,"y",y)
 	xmlNodeSetAttribute(newRect,"width",width)
 	xmlNodeSetAttribute(newRect,"height",height)
-	xmlNodeSetAttribute(newRect,"style","fill:rgb(255,255,255)")
+	xmlNodeSetAttribute(newRect,"rx",rx or 0)
+	xmlNodeSetAttribute(newRect,"ry",ry or 0)
 	dgsElementData[svg].svgDocumentUpdate = true
 	return newRect
 end

commit 77605e7d8a7b0f2086716dc9d5ed3971b958c149

Fix button alpha
parent alpha does not affect the button - caused by https://github.com/thisdp/dgs/commit/a4e4884c471489b2f229d6fc9ce34344b8f73a27

Core/button.lua
@@ -153,18 +153,18 @@ dgsRenderer["dgs-dxbutton"] = function(source,x,y,w,h,mx,my,cx,cy,enabledInherit
 			local targetColor = colors[buttonState]
 			if eleData.colorTransitionPeriod > 0 then
 				renderBuffer.currentColor = interpolateColor(renderBuffer.startColor or targetColor,targetColor,(getTickCount()-eleData.currentStateTick)/eleData.colorTransitionPeriod) -- todo
-				finalcolor = renderBuffer.currentColor
+				finalcolor = applyColorAlpha(renderBuffer.currentColor,parentAlpha)
 			else
-				finalcolor = targetColor
+				finalcolor = applyColorAlpha(targetColor,parentAlpha)
 			end
 		end
 	else
 		local targetColor = colors[buttonState]
 		if eleData.colorTransitionPeriod > 0 and getTickCount()-eleData.currentStateTick <= eleData.colorTransitionPeriod then
 			renderBuffer.currentColor = interpolateColor(renderBuffer.startColor or targetColor,targetColor,(getTickCount()-eleData.currentStateTick)/eleData.colorTransitionPeriod) -- todo
-			finalcolor = renderBuffer.currentColor
+			finalcolor = applyColorAlpha(renderBuffer.currentColor,parentAlpha)
 		else
-			finalcolor = targetColor
+			finalcolor = applyColorAlpha(targetColor,parentAlpha)
 		end
 	end
 	------------------------------------