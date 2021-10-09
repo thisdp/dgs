9d92ba3c23a7c38d6ef242ad4f64c41e47c915d6
thisdp <30386052+thisdp@users.noreply.github.com>
2021-10-09 16:10:21 +0800

    Update screenSource.lua

--- plugin/screenSource.lua
+++ plugin/screenSource.lua
@@ -4,15 +4,12 @@ GlobalScreenSourceHeightFactor = 1
 function dgsCreateScreenSource(uPos,vPos,uSize,vSize,relative)
 	local ss = createElement("dgs-dxscreensource")
 	dgsSetData(ss,"asPlugin","dgs-dxscreensource")
-	dgsSetData(ss,"customUV",true)
 	if not uPos or not vPos or not uSize or not vSize then
-		dgsSetData(ss,"uvPos",{0,0,true})
-		dgsSetData(ss,"uvSize",{1,1,true})
-		dgsSetData(ss,"customUV",false)
+		dgsSetData(ss,"uvPos",{nil,nil,false})
+		dgsSetData(ss,"uvSize",{nil,nil,false})
 	else
 		dgsSetData(ss,"uvPos",{uPos,vPos,relative or false})
 		dgsSetData(ss,"uvSize",{uSize,vSize,relative or false})
-		dgsSetData(ss,"customUV",true)
 	end
 	triggerEvent("onDgsPluginCreate",ss,sourceResource)
 	if not isElement(GlobalScreenSource) then
@@ -23,8 +20,6 @@ end
 
 function dgsScreenSourceSetUVPosition(ss,uPos,vPos,relative)
 	if not(dgsGetPluginType(ss) == "dgs-dxscreensource") then error(dgsGenAsrt(ss,"dgsScreenSourceSetUVPosition",1,"dgs-dxscreensource")) end
-	if not(type(uPos) == "number") then error(dgsGenAsrt(uPos,"dgsScreenSourceSetUVPosition",1,"number")) end
-	if not(type(vPos) == "number") then error(dgsGenAsrt(vPos,"dgsScreenSourceSetUVPosition",2,"number")) end
 	dgsSetData(ss,"uvPos",{uPos,vPos,relative or false})
 	return true
 end
@@ -33,25 +28,26 @@ function dgsScreenSourceGetUVPosition(ss,relative)
 	if not(dgsGetPluginType(ss) == "dgs-dxscreensource") then error(dgsGenAsrt(ss,"dgsScreenSourceSetUVPosition",1,"dgs-dxscreensource")) end
 	local relative = relative or false
 	local uvPos = dgsElementData[ss].uvPos
-	if relative then
-		if uvPos[3] == relative then
-			return uvPos[1],uvPos[2]
+	if uvPos[1] and uvPos[2] then
+		if relative then
+			if uvPos[3] == relative then
+				return uvPos[1],uvPos[2]
+			else
+				return uvPos[1]/(sW*GlobalScreenSourceWidthFactor),uvPos[2]/(sH*GlobalScreenSourceHeightFactor)
+			end
 		else
-			return uvPos[1]/(sW*GlobalScreenSourceWidthFactor),uvPos[2]/(sH*GlobalScreenSourceHeightFactor)
-		end
-	else
-		if uvPos[3] == relative then
-			return uvPos[1],uvPos[2]
-		else
-			return uvPos[1]*sW*GlobalScreenSourceWidthFactor,uvPos[2]*sH*GlobalScreenSourceHeightFactor
+			if uvPos[3] == relative then
+				return uvPos[1],uvPos[2]
+			else
+				return uvPos[1]*sW*GlobalScreenSourceWidthFactor,uvPos[2]*sH*GlobalScreenSourceHeightFactor
+			end
 		end
 	end
+	return false,false
 end
 
 function dgsScreenSourceSetUVSize(ss,uSize,vSize,relative)
 	if not(dgsGetPluginType(ss) == "dgs-dxblurbox") then error(dgsGenAsrt(ss,"dgsScreenSourceSetUVSize",1,"dgs-dxblurbox")) end
-	if not(type(w) == "number") then error(dgsGenAsrt(w,"dgsScreenSourceSetUVSize",2,"number")) end
-	if not(type(h) == "number") then error(dgsGenAsrt(h,"dgsScreenSourceSetUVSize",3,"number")) end
 	dgsSetData(ss,"uvSize",{uSize,vSize,relative or false})
 	return true
 end
@@ -60,25 +56,28 @@ function dgsScreenSourceGetUVSize(ss,relative)
 	if not(dgsGetPluginType(ss) == "dgs-dxscreensource") then error(dgsGenAsrt(ss,"dgsScreenSourceGetUVSize",1,"dgs-dxscreensource")) end
 	local relative = relative or false
 	local uvSize = dgsElementData[ss].uvSize
-	if relative then
-		if uvSize[3] == relative then
-			return uvSize[1],uvSize[2]
+	if uvSize[1] and uvSize[2] then
+		if relative then
+			if uvSize[3] == relative then
+				return uvSize[1],uvSize[2]
+			else
+				return uvSize[1]/(sW*GlobalScreenSourceWidthFactor),uvSize[2]/(sH*GlobalScreenSourceHeightFactor)
+			end
 		else
-			return uvSize[1]/(sW*GlobalScreenSourceWidthFactor),uvSize[2]/(sH*GlobalScreenSourceHeightFactor)
-		end
-	else
-		if uvSize[3] == relative then
-			return uvSize[1],uvSize[2]
-		else
-			return uvSize[1]*sW*GlobalScreenSourceWidthFactor,uvSize[2]*sH*GlobalScreenSourceHeightFactor
+			if uvSize[3] == relative then
+				return uvSize[1],uvSize[2]
+			else
+				return uvSize[1]*sW*GlobalScreenSourceWidthFactor,uvSize[2]*sH*GlobalScreenSourceHeightFactor
+			end
 		end
 	end
+	return false,false
 end
 
 dgsCustomTexture["dgs-dxscreensource"] = function(posX,posY,width,height,u,v,usize,vsize,image,rotation,rotationX,rotationY,color,postGUI)
-	if dgsElementData[image].customUV then
-		local uvPos = dgsElementData[image].uvPos
-		local uvSize = dgsElementData[image].uvSize
+	local uvPos = dgsElementData[image].uvPos
+	local uvSize = dgsElementData[image].uvSize
+	if uvPos[1] and uvPos[2] and uvSize[1] and uvSize[2] then
 		local uPos,vPos = uvPos[3] and uvPos[1]*GlobalScreenSourceWidthFactor or uvPos[1], uvPos[3] and uvPos[2]*GlobalScreenSourceHeightFactor or uvPos[2]
 		local uSize,vSize = uvSize[3] and uvSize[1]*GlobalScreenSourceWidthFactor or uvSize[1], uvSize[3] and uvSize[2]*GlobalScreenSourceHeightFactor or uvSize[2]
 		__dxDrawImageSection(posX,posY,width,height,uPos,vPos,uSize,vSize,GlobalScreenSource,rotation,rotationX,rotationY,color,postGUI)
