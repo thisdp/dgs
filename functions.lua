516365033846755f300e3f7df52409d8890a9ca0
thisdp <30386052+thisdp@users.noreply.github.com>
2022-02-23 20:46:46 +0800

    fix view position and real position mismatch in scroll pane

--- functions.lua
+++ functions.lua
@@ -100,9 +100,10 @@ function getParentLocation(dgsEle,rndSuspend,x,y,includeSide)
 			local size = eleData.absSize
 			local relSizX,relSizY = size[1]-(dgsElementData[ scrollbar[1] ].visible and scbThick or 0),size[2]-(dgsElementData[ scrollbar[2] ].visible and scbThick or 0)
 			local maxSize = eleData.maxChildSize
-			local maxX,maxY = (maxSize[1]-relSizX),(maxSize[2]-relSizY)
-			maxX,maxY = maxX > 0 and maxX or 0,maxY > 0 and maxY or 0
-			x,y = x+absPosX-maxX*dgsElementData[scrollbar[2]].position*0.01,y+absPosY-maxY*dgsElementData[scrollbar[1]].position*0.01
+			x,y = x+absPosX-eleData.horizontalMoveOffsetTemp,y+absPosY-eleData.verticalMoveOffsetTemp
+			--local maxX,maxY = (maxSize[1]-relSizX),(maxSize[2]-relSizY)
+			--maxX,maxY = maxX > 0 and maxX or 0,maxY > 0 and maxY or 0
+			--x,y = x+absPosX-maxX*dgsElementData[scrollbar[2]].position*0.01,y+absPosY-maxY*dgsElementData[scrollbar[1]].position*0.01
 		elseif dgsElementType[dgsEle] == "dgs-dxscalepane" then
 			--[[local scrollbar = eleData.scrollbars
 			local scbThick = eleData.scrollBarThick
