902b3155bf482eec50618898f7b8f0dd1127e895
thisdp <30386052+thisdp@users.noreply.github.com>
2022-05-30 18:46:15 +0800

    Update progressbar.lua

--- Core/progressbar.lua
+++ Core/progressbar.lua
@@ -446,6 +446,10 @@ float4 myShader(float2 tex:TEXCOORD0,float4 color:COLOR0):COLOR0{
 
 technique DrawCircle{
 	pass P0	{
+		//Solve Render Issues
+		SeparateAlphaBlendEnable = true;
+		SrcBlendAlpha = One;
+		DestBlendAlpha = InvSrcAlpha;
 		PixelShader = compile ps_2_a myShader();
 	}
 }
@@ -534,6 +538,10 @@ float4 circleShader(float2 tex:TEXCOORD0,float4 color:COLOR0):COLOR0{
 
 technique circleTechnique{
 	pass p0{
+		//Solve Render Issues
+		SeparateAlphaBlendEnable = true;
+		SrcBlendAlpha = One;
+		DestBlendAlpha = InvSrcAlpha;
 		PixelShader = compile ps_2_a circleShader();
 	}
 }
