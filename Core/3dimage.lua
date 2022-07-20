0fedfc13f7000c1b234dda95266430f93fd4a592
thisdp <30386052+thisdp@users.noreply.github.com>
2022-07-20 23:57:14 +0800

    fix

--- Core/3dimage.lua
+++ Core/3dimage.lua
@@ -224,7 +224,7 @@ dgsRenderer["dgs-dx3dimage"] = function(source,x,y,w,h,mx,my,cx,cy,enabledInheri
 	if isRender then
 		local maxDistance = eleData.maxDistance
 		local camX,camY,camZ = cameraPos[1],cameraPos[2],cameraPos[3]
-		local dx,dy,dz = camX-wx,camY-wy,camZ-mz
+		local dx,dy,dz = camX-wx,camY-wy,camZ-wz
 		local distance = (dx*dx+dy*dy+dz*dz)^0.5
 		if distance <= maxDistance and distance > 0 then
 			local canBeBlocked = eleData.canBeBlocked
