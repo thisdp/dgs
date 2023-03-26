thisdp/dgs

commit 1c8b1eae928b42b5e0830e121af826325888229d

Improve server-side messages
This will fix issues with the messages on the server, whether they are typos, unclear sentences, or grammatical errors. Additionally, I have added a new message when updating to ensure that people read about the releases before updating. 

This will also add a new function outputDGSMessage, which can help ensure that all server messages have a consistent style.

update.lua
@@ -11,7 +11,7 @@ if not DGSConfig.enableUpdateSystem then return end
 local _fetchRemote = fetchRemote
 function fetchRemote(...)
 	if not hasObjectPermissionTo(getThisResource(),"function.fetchRemote",true) then
-		outputDebugString("[DGS]Request 'fetchRemote', but access denied. Use the command 'aclrequest allow dgs all'",2)
+		outputDGSMessage("fetchRemote' was called, but access was denied. To resolve this issue, use the command 'aclrequest allow dgs all'.",2)
 		return false
 	end
 	return _fetchRemote(...)
@@ -22,49 +22,39 @@ ManualUpdate = false
 updateTimer = false
 updatePeriodTimer = false
 function checkUpdate()
-	outputDebugString("[DGS]Connecting to github...")
+	outputDGSMessage("Checking for updates..",nil,"Updater")
 	fetchRemote("https://raw.githubusercontent.com/thisdp/dgs/master/update.cfg",function(data,err)
 		if err == 0 then
 			RemoteVersion = tonumber(data)
 			if not ManualUpdate then
 				if RemoteVersion > version then
-					outputDebugString("[DGS]Remote Version Got [Remote:"..data.." Current:"..version.."].")
-					outputDebugString("[DGS]Update? Command: "..DGSConfig.updateCommand)
+					outputDGSMessage("New update available: "..version.." > "..data..". Consider updating your DGS using /"..DGSConfig.updateCommand,nil,"Updater")
+					outputDGSMessage("Please check the changelogs at https://github.com/thisdp/dgs/releases to avoid breaking changes.",nil,"Updater")
 					if isTimer(updateTimer) then killTimer(updateTimer) end
 					updateTimer = setTimer(function()
 						if RemoteVersion > version then
-							outputDebugString("[DGS]Remote Version Got [Remote:"..RemoteVersion.." Current:"..version.."].")
-							outputDebugString("[DGS]Update? Command: "..DGSConfig.updateCommand)
+							outputDGSMessage("New update available: "..version.." > "..data..". Consider updating your DGS using /"..DGSConfig.updateCommand,nil,"Updater")
+							outputDGSMessage("Please check the changelogs at https://github.com/thisdp/dgs/releases before to avoid breaking changes.",nil,"Updater")
 						else
 							killTimer(updateTimer)
 						end
 					end,DGSConfig.updateCheckNoticeInterval*60000,0)
 				else
-					outputDebugString("[DGS]Current Version("..version..") is the latest!")
+					outputDGSMessage("Current version ("..version..") is the latest!",nil,"Updater")
 				end
 			else
 				startUpdate()
 			end
 		else
-			outputDebugString("[DGS]Can't Get Remote Version ("..err..")")
+			outputDGSMessage("The remote version could not be retrieved ("..err..")",nil,"Updater",2)
 		end
 	end)
 end
 
 function checkServerVersion(player)
-	if getVersion().sortable < "1.5.4-9.11342" then
-		if player then
-			local acc = getPlayerAccount(player)
-			if acc and getAccountName(acc) == "Console" then
-				outputDebugString("[DGS]Your server version is outdated upgrade to 1.5.4-9.11342 or higher",2)
-			else
-				outputChatBox("[DGS]Your server version is outdated upgrade to 1.5.4-9.11342 or higher",player,255,255,0)
-			end
-			return false
-		else
-			outputDebugString("[DGS]Your server version is outdated upgrade to 1.5.4-9.11342 or higher",2)
-			return false
-		end
+	if getVersion().sortable > "1.5.4-9.11342" then
+		outputDGSMessage("Your server version is too old to support dgs update system.",player,nil,2)
+		return false
 	end
 	return true
 end
@@ -87,9 +77,8 @@ addCommandHandler(DGSConfig.updateCommand,function(player)
 		isPermit = isPermit or (consoleGroup and isObjectInACLGroup("user."..accName,consoleGroup))
 	end
 	if isPermit then
-		outputDebugString("[DGS]"..getPlayerName(player).." attempt to update dgs (Allowed)")
-		outputDebugString("[DGS]Preparing for updating dgs")
-		outputChatBox("[DGS]Preparing for updating dgs",root,0,255,0)
+		outputDGSMessage(getPlayerName(player).." attempt to update dgs (Allowed)")
+		outputDGSMessage("Preparing to update dgs",{player,"console"},"Updater")
 		if RemoteVersion > version then
 			startUpdate()
 		else
@@ -97,28 +86,28 @@ addCommandHandler(DGSConfig.updateCommand,function(player)
 			checkUpdate()
 		end
 	else
-		outputChatBox("[DGS]Access Denined!",player,255,0,0)
-		outputDebugString("[DGS]"..getPlayerName(player).." attempt to update dgs (Denied)!",2)
+		outputDGSMessage("Access Denied!",player,"Updater",1)
+		outputDGSMessage(getPlayerName(player).." attempt to update dgs (Denied)",nil,"Updater",2)
 	end
 end)
 
 function startUpdate()
 	ManualUpdate = false
 	setTimer(function()
-		outputDebugString("[DGS]Requesting Update Data (From github)...")
+		outputDGSMessage("Requesting update data (From GitHub)...",nil,"Updater")
 		fetchRemote("https://raw.githubusercontent.com/thisdp/dgs/master/meta.xml",function(data,err)
 			if err == 0 then
-				outputDebugString("[DGS]Update Data Acquired")
+				outputDGSMessage("Update data retrieved successfully.",nil,"Updater")
 				if fileExists("updated/meta.xml") then
 					fileDelete("updated/meta.xml")
 				end
 				local meta = fileCreate("updated/meta.xml")
 				fileWrite(meta,data)
 				fileClose(meta)
-				outputDebugString("[DGS]Requesting Verification Data...")
+				outputDGSMessage("Requesting verification data...",nil,"Updater")
 				getGitHubTree()
 			else
-				outputDebugString("[DGS]Can't Get Remote Update Data (ERROR:"..err..")",2)
+				outputDGSMessage("Unable to retrieve remote update data ("..err..")",nil,"Updater",2)
 			end
 		end)
 	end,50,1)
@@ -149,7 +138,7 @@ function getGitHubTree(path,nextPath)
 				checkFiles()
 			end
 		else
-			outputDebugString("[DGS]Failed To Get Verification Data, Please Try Again Later (API Cool Down 60 mins)!",2)
+			outputDGSMessage("Failed to get verification data, please try again later (API Cool Down 60 mins)",nil,"Updater",2)
 		end
 	end)
 end
@@ -172,7 +161,7 @@ function checkFiles()
 				sha = hash("sha1","blob " .. size .. "\0" ..text)
 			end
 			if sha ~= fileHash[path] then
-				outputDebugString("[DGS]Update Required: ("..path..")")
+				outputDGSMessage("Update required: ("..path..")",nil,"Updater")
 				table.insert(preUpdate,path)
 			end
 		end
@@ -188,7 +177,7 @@ function DownloadFiles()
 		DownloadFinish()
 		return
 	end
-	outputDebugString("[DGS]Requesting ("..UpdateCount.."/"..(#preUpdate or "Unknown").."): "..tostring(preUpdate[UpdateCount]).."")
+	outputDGSMessage("Requesting ("..UpdateCount.."/"..(#preUpdate or "Unknown").."): "..tostring(preUpdate[UpdateCount]),nil,"Updater")
 	fetchRemote("https://raw.githubusercontent.com/thisdp/dgs/master/"..preUpdate[UpdateCount],function(data,err,path)
 		if err == 0 then
 			local size = 0
@@ -202,9 +191,9 @@ function DownloadFiles()
 			fileWrite(file,data)
 			local newsize = fileGetSize(file)
 			fileClose(file)
-			outputDebugString("[DGS]File Got ("..UpdateCount.."/"..#preUpdate.."): "..path.." [ "..size.."B -> "..newsize.."B ]")
+			outputDGSMessage("Got ("..UpdateCount.."/"..#preUpdate.."): "..path.." [ "..size.."B -> "..newsize.."B ]",nil,"Updater")
 		else
-			outputDebugString("[DGS]Download Failed: "..path.." ("..err..")!",2)
+			outputDGSMessage("Download failed: "..path.." ("..err..")!",nil,"Updater",2)
 		end
 		if preUpdate[UpdateCount+1] then
 			DownloadFiles()
@@ -215,7 +204,7 @@ function DownloadFiles()
 end
 
 function DownloadFinish()
-	outputDebugString("[DGS]Changing Config File")
+	outputDGSMessage("Updating version file",nil,"Updater")
 	if fileExists("update.cfg") then
 		fileDelete("update.cfg")
 	end
@@ -240,9 +229,8 @@ function DownloadFinish()
 		xmlSaveFile(xml)
 		xmlUnloadFile(xml)
 	end
-	outputDebugString("[DGS]Update Complete ( "..#preUpdate.." File"..(#preUpdate==1 and "" or "s").." Changed )")
-	outputDebugString("[DGS]Please Restart DGS")
-	outputChatBox("[DGS]Update Complete ( "..#preUpdate.." File"..(#preUpdate==1 and "" or "s").." Changed )",root,0,255,0)
+	outputDGSMessage("Update successful: "..#preUpdate.." file"..(#preUpdate==1 and "" or "s").." have been updated.",{root,"console"},"Updater")
+	outputDGSMessage("Please restart DGS",nil,"Updater")
 	preUpdate = {}
 	UpdateCount = 0
 end
@@ -255,19 +243,12 @@ addCommandHandler("dgsver",function(pla,cmd)
 		fileClose(file)
 		vsdd = tonumber(vscd)
 		if vsdd then
-			outputDebugString("[DGS]Version: "..vsdd,3)
+			outputDGSMessage("Current version: "..vsdd,pla)
 		else
-			outputDebugString("[DGS]Version State is damaged! Please use /"..DGSConfig.updateCommand.." to update",1)
+			outputDGSMessage("Version file is damaged! Please use /"..DGSConfig.updateCommand.." to update",pla,nil,2)
 		end
 	else
-		outputDebugString("[DGS]Version State is damaged! Please use /"..DGSConfig.updateCommand.." to update",1)
-	end
-	if getPlayerName(pla) ~= "Console" then
-		if vsdd then
-			outputChatBox("[DGS]Version: "..vsdd,pla,0,255,0)
-		else
-			outputChatBox("[DGS]Version State is damaged! Please use /"..DGSConfig.updateCommand.." to update",pla,255,0,0)
-		end
+		outputDGSMessage("Version file is damaged! Please use /"..DGSConfig.updateCommand.." to update",pla,nil,2)
 	end
 end)
 
@@ -277,7 +258,7 @@ function backupStyleMapper()
 	if DGSConfig.enableMetaBackup then
 		fileCopy("meta.xml","meta.xml.bak",true)
 	end
-	assert(fileExists("meta.xml"),"[DGS] Please rename the meta xml as meta.xml")
+	if not fileExists("meta.xml") then return outputDGSMessage("Please rename the meta xml as meta.xml",nil,"Updater",2) end
 	local meta = fileOpen("meta.xml")
 	local str = fileRead(meta,fileGetSize(meta))
 	local startStr = "<!----$Add Your Styles Here---->"
@@ -297,7 +278,7 @@ function backupStyleMapper()
 end
 
 function recoverStyleMapper()
-	assert(styleBackupStr ~= "","[DGS] Failed to recover style mapper")
+	if styleBackupStr == "" then return outputDGSMessage("Failed to recover style mapper",nil,"Updater",2) end
 	local meta = fileOpen("updated/meta.xml")
 	local str = fileRead(meta,fileGetSize(meta))
 	fileClose(meta)

G2DManager_s.lua
@@ -69,9 +69,9 @@ addCommandHandler("g2d",function(player,command,...)
 							end
 						end
 					end
-					outputDebugString("[DGS-G2D] Selected "..tableCount(G2D.select).." resources, to see the selections, command: g2d list")
+					outputDGSMessage("Selected "..tableCount(G2D.select).." resources, to see the selections, command: g2d list",nil,"G2D")
 				else
-					outputDebugString("[DGS-G2D] Selected 0 resources, to see the selections, command: g2d list")
+					outputDGSMessage("Selected 0 resources, to see the selections, command: g2d list",nil,"G2D")
 				end
 			elseif args[1] == "add" then
 				if args[2] and args[2] ~= "" then
@@ -90,32 +90,32 @@ addCommandHandler("g2d",function(player,command,...)
 							end
 						end
 					end
-					outputDebugString("[DGS-G2D] Selected "..tableCount(G2D.select).." resources, to see the selections, command: g2d list")
+					outputDGSMessage("Selected "..tableCount(G2D.select).." resources, to see the selections, command: g2d list",nil,"G2D")
 				else
-					outputDebugString("[DGS-G2D] Selected 0 resources, to see the selections, command: g2d list")
+					outputDGSMessage("Selected 0 resources, to see the selections, command: g2d list")
 				end
 			elseif args[1] == "list" then
-				outputDebugString("[DGS-G2D] There are "..tableCount(G2D.select).." resources selected:")
+				outputDGSMessage("There are "..tableCount(G2D.select).." resources selected:",nil,"G2D")
 				for k,v in pairs(G2D.select) do
 					outputDebugString(k)
 				end
 			elseif args[1] == "clear" then
 				G2D.select = {}
-				outputDebugString("[DGS-G2D] Selections cleared!")
+				outputDGSMessage("Selections cleared!",nil,"G2D")
 			elseif args[1] == "start" then
 				if not G2D.Process then
 					G2D.Process = true
 					G2D.Running = {}
 					G2DStart()
 				else
-					print("[DGS-G2D]G2D is running!")
+					outputDGSMessage("G2D is running!",nil,"G2D")
 				end
 			elseif args[1] == "stop" then
 				if G2D.Process then
-					print("[DGS-G2D]G2D process terminated!")
+					outputDGSMessage("G2D process terminated!",nil,"G2D")
 					G2D.Process = false
 				else
-					print("[DGS-G2D]G2D is not running!")
+					outputDGSMessage("G2D is not running!",nil,"G2D")
 				end
 			elseif args[1] == "crawl" then
 				if not checkServerVersion() then return end
@@ -127,32 +127,32 @@ addCommandHandler("g2d",function(player,command,...)
 					elseif args[2] == "sublime" then
 						CrawlWikiFromMTA("sublime")
 					else
-						print("[DGS]Current type is not supported!")
+						outputDGSMessage("Current type is not supported!",nil,"Crawl")
 					end
 				else
-					print("[DGS]Please select target type: g2d -g <npp/vsc/sublime>")
+					outputDGSMessage("Please select target type: g2d -g <npp/vsc/sublime>",nil,"Crawl")
 				end
 			elseif args[1] == "type" then
 				if args[2] then
 					if not G2D.Process then
 						if args[2] == "convertor" then
 							G2D.type = "convertor"
-							print("[DGS-G2D]Current G2D type has been changed to "..G2D.type)
+							outputDGSMessage("Current G2D type has been changed to "..G2D.type,nil,"G2D")
 						elseif args[2] == "hooker" then
 							G2D.type = "hooker"
-							print("[DGS-G2D]Current G2D type has been changed to "..G2D.type)
+							outputDGSMessage("Current G2D type has been changed to "..G2D.type,nil,"G2D")
 						else
-							print("[DGS-G2D]Bad G2D type, expected convertor/hooker got "..args[2])
+							outputDGSMessage("Bad G2D type, expected convertor/hooker got "..args[2],nil,"G2D")
 						end
 					else
-						print("[DGS-G2D]G2D type can not be changed while G2D process is running")
+						outputDGSMessage("G2D type can not be changed while G2D process is running",nil,"G2D")
 					end
 				else
-					print("[DGS-G2D]Current G2D type is "..G2D.type)
+					outputDGSMessage("Current G2D type is "..G2D.type,nil,"G2D")
 				end
 			else
-				outputDebugString("[DGS-G2D]Command help")
-				outputDebugString("Option		Arguments		Comment")
+				outputDGSMessage("Command help",nil,"G2D")
+				outputDGSMessage("Option		Arguments		Comment")
 				for i=1,#G2DHelp do
 					local items = G2DHelp[i]
 					outputDebugString(items[1].."		"..items[2].."		"..items[3])
@@ -432,7 +432,7 @@ convertEventTable = {
 
 
 function showProgress(progress)
-	print("[G2D]Progress "..string.format("%.2f",progress).."%")
+	outputDGSMessage("Progress "..string.format("%.2f",progress).."%",nil,"G2D")
 end
 
 G2DRunningData = {
@@ -442,7 +442,7 @@ G2DRunningData = {
 
 function G2DStart()
 	if G2D.type == "convertor" then
-		print("[DGS-G2D]Scanning files...")
+		outputDGSMessage("Scanning files...",nil,"G2D")
 		local process = {}
 		for resN,res in pairs(G2D.select) do
 			local xml = xmlLoadFile(":"..resN.."/meta.xml")
@@ -453,18 +453,18 @@ function G2DStart()
 				end
 			end
 		end
-		print("[DGS-G2D]"..#process.." files to be converted")
+		outputDGSMessage(#process.." files to be converted",nil,"G2D")
 		G2D.Files = process
 		G2D.Running = coroutine.create(function()
 			G2D.StartTick = getTickCount()
 			for i=1,#G2D.Files do
 				processFileConvertor(G2D.File[i])
 			end
-			print("[DGS-G2D]Process Done")
+			outputDGSMessage("Process Done",nil,"G2D")
 			G2D.Process = false
 		end)
 	elseif G2D.type == "hooker" then
-		print("[DGS-G2D]Scanning resources...")
+		outputDGSMessage("Scanning resources...",nil,"G2D")
 		G2D.Running = coroutine.create(function()
 			G2D.StartTick = getTickCount()
 			for resN,res in pairs(G2D.select) do
@@ -477,7 +477,7 @@ function G2DStart()
 						local str = fileRead(file,fileGetSize(file))
 						local backupStr = str
 						if str:find("guiCreate") then	--has gui create function and is the 1st script in client
-							print("[DGS-G2D]Processing "..path)
+							outputDGSMessage("Processing "..path,nil,"G2D")
 							local findA,findB = string.find(str,"loadstring%s*%(.*dgsG2DLoadHooker%s*%(%s*%)%s*%)")
 							if not findA then	--if not, add
 								fileSetPos(file,0)
@@ -502,7 +502,7 @@ function G2DStart()
 					end
 				end
 			end
-			print("[DGS-G2D]Process Done")
+			outputDGSMessage("The process has finished successfully",nil,"G2D")
 			G2D.Process = false
 		end)
 	end
@@ -520,7 +520,7 @@ function processExpired()
 end
 
 function processFileConvertor(filename)
-	print("[G2D]Start to process file '"..filename.."'")
+	outputDGSMessage("Start to process file '"..filename.."'",nil,"G2D")
 	local file = fileOpen(filename)
 	local str = fileRead(file,fileGetSize(file))
 	local utf8BOM = false
@@ -537,7 +537,7 @@ function processFileConvertor(filename)
 	ls:start()
 	local az = AnalyzerState(ls.result)
 	local convTabCnt = #convertFunctionTable
-	print("[G2D]Replacing Functions")
+	outputDGSMessage("Replacing Functions",nil,"G2D")
 	for i=1,convTabCnt do
 		az:set(convertFunctionTable[i])
 		az:executeProcess()
@@ -548,7 +548,7 @@ function processFileConvertor(filename)
 		end
 	end
 	local convTabCnt = #convertEventTable
-	print("[G2D]Replacing Events")
+	outputDGSMessage("Replacing Events",nil,"G2D")
 	for i=1,convTabCnt do
 		az:set(convertEventTable[i],true)
 		az:executeProcess()
@@ -565,7 +565,7 @@ end
 
 
 function processFileHooker(filename)
-	print("[G2D]Start to process file '"..filename.."'")
+	outputDGSMessage("Start to process file '"..filename.."'",nil,"G2D")
 	local file = fileOpen(filename)
 	local str = fileRead(file,fileGetSize(file))
 	local utf8BOM = false
@@ -582,7 +582,7 @@ function processFileHooker(filename)
 	ls:start()
 	local az = AnalyzerState(ls.result)
 	local convTabCnt = #convertFunctionTable
-	print("[G2D]Replacing Functions")
+	outputDGSMessage("Replacing Functions",nil,"G2D")
 	for i=1,convTabCnt do
 		az:set(convertFunctionTable[i])
 		az:executeProcess()
@@ -593,7 +593,7 @@ function processFileHooker(filename)
 		end
 	end
 	local convTabCnt = #convertEventTable
-	print("[G2D]Replacing Events")
+	outputDGSMessage("Replacing Events",nil,"G2D")
 	for i=1,convTabCnt do
 		az:set(convertEventTable[i],true)
 		az:executeProcess()
@@ -616,11 +616,11 @@ local mainWikiURL = "https://wiki.multitheftauto.com"
 local threadPoolSize = 2
 function CrawlWikiFromMTA(t)
 	local targetURL = mainWikiURL.."/wiki/Template:DGSFUNCTIONS"
-	print("[DGS]Crawling wiki...")
+	outputDGSMessage("Crawling wiki..",nil,"DGS")
 	fetchRemote(targetURL,{},function(data,info,t)
 		if info.success then
 			local startPos = 0
-			print("[DGS]Wiki data is ready, Reading...")
+			outputDGSMessage("Wiki data has been retrieved. Reading now..",nil,"G2D")
 			local fncList = {type=t}
 			while(true) do
 				liStart_1,liStart_2 = string.find(data,"%<li%>",startPos)
@@ -639,8 +639,9 @@ function CrawlWikiFromMTA(t)
 				startPos = liEnd_2
 				xmlReleaseTempFiles(xmlNode)
 			end
-			print("[DGS]Function list("..#fncList..") is ready, Crawling...")
-			local fRProg = {thread=0,index=0,valid=0,progress=0,total=#fncList}
+			local fncListLen = #fncList
+			outputDGSMessage(fncList.." function"..fncListLen > 1 and "s" or "".." has been found. Crawling..",nil,"G2D")
+			local fRProg = {thread=0,index=0,valid=0,progress=0,total=fncListLen}
 			local fncData = {}
 			setTimer(function()
 				if fRProg.progress < fRProg.total then
@@ -663,7 +664,7 @@ function CrawlWikiFromMTA(t)
 										fRProg.thread = fRProg.thread-1
 										fRProg[poolID] = false
 										if info.success then
-											print("[DGS]Recorded ("..fRProg.progress.."/"..fRProg.total..")["..fncList[index].name.."]")
+											outputDGSMessage("Recorded ("..fRProg.progress.."/"..fRProg.total..")["..fncList[index].name.."]",nil,"Crawl")
 											local startPos = data:find("%<textarea")
 											local _,endPos = data:find("%<%/textarea>",startPos)
 											local line = data:sub(startPos,endPos)
@@ -677,7 +678,7 @@ function CrawlWikiFromMTA(t)
 											local targetSyntax = targetSyntax:gsub("\r",""):gsub("\n","")
 											fncList[index].syntax = targetSyntax
 										else
-											print("[DGS]Failed to get remote wiki data ("..info.statusCode ..")")
+											outputDGSMessage("Failed to get remote wiki data ("..info.statusCode ..")",nil,"Crawl")
 										end
 									end,{poolID,fRProg.index})
 									fRProg.valid = fRProg.valid+1
@@ -688,7 +689,7 @@ function CrawlWikiFromMTA(t)
 						end
 					end
 				else
-					print("[DGS]Crawling stage complete [Total:"..fRProg.total.."/Valid:"..fRProg.valid.."]")
+					outputDGSMessage("Crawling stage complete [Total:"..fRProg.total.."/Valid:"..fRProg.valid.."]",nil,"Crawl")
 					killTimer(sourceTimer)
 					fncList.valid = fRProg.valid
 					AnalyzeFunction(fncList)
@@ -699,14 +700,14 @@ function CrawlWikiFromMTA(t)
 end
 
 function AnalyzeFunction(tab)
-	print("[DGS]Start to analyze syntax")
+	outputDGSMessage("Start to analyze syntax",nil,"Crawl")
 	local nTable = {}
 	local validCount = 0
 	for i=1,#tab do
 		local item = tab[i]
 		if item.syntax then
 			validCount = validCount+1
-			print("[DGS]Analyzing syntax ("..validCount.."/"..tab.valid..")["..item.name.."]")
+			outputDGSMessage("Analyzing syntax ("..validCount.."/"..tab.valid..")["..item.name.."]",nil,"Crawl")
 			local startPos,endPos = string.find(item.syntax,item.name)
 			local rets = split(item.syntax:sub(1,startPos-1)," ")
 			local argStr = item.syntax:sub(endPos+1):gsub("%(",""):gsub("%)","")
@@ -743,7 +744,7 @@ function AnalyzeFunction(tab)
 			table.insert(nTable,resultTable)
 		end
 	end
-	print("[DGS]Syntax analyzing stage done")
+	outputDGSMessage("Syntax analyzing stage done",nil,"Crawl")
 	if tab.type == "npp" then
 		GenerateNPPAutoComplete(nTable)
 	elseif tab.type == "vsc" then
@@ -755,7 +756,7 @@ end
 
 function GenerateNPPAutoComplete(tab)
 	if fileExists("nppAC4DGS.xml") then fileDelete("nppAC4DGS.xml") end
-	print("[DGS]Generating NPP autocomplete file...")
+	outputDGSMessage("Generating NPP autocomplete file..",nil,"Crawl")
 	local xml = xmlCreateFile("nppAC4DGS.xml","NotepadPlus")
 	local envNode = xmlCreateChild(xml,"Environment")
 	local acNode = xmlCreateChild(xml,"AutoComplete")
@@ -789,12 +790,12 @@ function GenerateNPPAutoComplete(tab)
 	fileSetPos(f,0)
 	fileWrite(f,str)
 	fileClose(f)
-	print("[DGS]NPP autocomplete file is saved as 'nppAC4DGS.xml'")
+	outputDGSMessage("NPP autocomplete file has been saved as 'nppAC4DGS.xml'",nil,"Crawl")
 end
 
 function GenerateVSCodeAutoComplete(tab)
 	if fileExists("dgs.code-snippets") then fileDelete("dgs.code-snippets") end
-	print("[DGS]Generating VSCode autocomplete file...")
+	outputDGSMessage("Generating VSCode autocomplete file..",nil,"Crawl")
 	local t = {}
 	for i=1,#tab do
 		local item = tab[i]
@@ -806,12 +807,12 @@ function GenerateVSCodeAutoComplete(tab)
 	local json = toJSON(t,false,"spaces")
 	fileWrite(f,json:sub(3, json:len() - 1))
 	fileClose(f)
-	print("[DGS]VSCode autocomplete file is saved as 'dgs.code-snippets'")
+	outputDGSMessage("VSCode autocomplete file has been saved as 'dgs.code-snippets'",nil,"Crawl")
 end
 
 function GenerateSublimeAutoComplete(tab)
 	if fileExists("dgs.sublime-completions") then fileDelete("dgs.sublime-completions") end
-	print("[DGS]Generating Sublime autocomplete file...")
+	outputDGSMessage("Generating Sublime autocomplete file..",nil,"Crawl")
 	local t = {scope = "source.lua",completions = {}}
 	for i=1,#tab do
 		local item = tab[i]
@@ -823,5 +824,5 @@ function GenerateSublimeAutoComplete(tab)
 	local json = toJSON(t,false,"spaces")
 	fileWrite(f,json:sub(3, json:len() - 1))
 	fileClose(f)
-	print("[DGS]Sublime autocomplete file is saved as 'dgs.sublime-completions'")
+	outputDGSMessage("Sublime autocomplete file has been saved as 'dgs.sublime-completions'",nil,"Crawl")
 end

server.lua
@@ -1,4 +1,25 @@
-﻿-----------Config Loader
+﻿function outputDGSMessage(message,visibleTo,title,level) -- level: 3 = info, 2 = warning, 1 = error, default is info
+	message = "[DGS"..(title and " "..title or "").."] "..message
+	if type(visibleTo) ~= "table" then
+		visibleTo = {visibleTo or "console"}
+	end
+	local r,g,b = 0,255,0 
+	if level == 2 then
+		r,g,b = 255, 147, 0
+	elseif level == 1 then
+		r,g,b = 255,0,0
+	end
+	for i=1,#visibleTo do
+		local to = visibleTo[i]
+		if to and (to ~= "console" and (not isElement(to) or getElementType(to) ~= "console")) then
+			outputChatBox(message,to,r,g,b)
+		else
+			outputDebugString(message,getVersion().sortable > "1.5.7-9.20477" and 4 or level,r,g,b)
+		end
+	end
+end
+
+-----------Config Loader
 DGSConfig = {
 	updateCheck						= true,			-- Enable:true;Disable:false
 	updateCheckInterval				= 120,			-- Minutes
@@ -13,6 +34,7 @@ DGSConfig = {
 	enableCompatibilityCheck	 	= true,			-- Enable compatibility check warnings
 	enableDebug 					= true,			-- Enable /debugdgs
 }
+
 
 function loadConfig()
 	if fileExists("config.txt") then 
@@ -33,10 +55,10 @@ function loadConfig()
 						DGSConfig[name] = dgsConfig[name]
 					end
 				end
-				outputDebugString("[DGS]Config Loaded!")
+				outputDGSMessage("The config file has been loaded.")
 			else
 				configUpdateRequired = true
-				outputDebugString("[DGS]Invalid config file!",3)
+				outputDGSMessage("Invalid config file.",3)
 			end
 			if configUpdateRequired then
 				fileDelete("config.txt")
@@ -48,10 +70,10 @@ function loadConfig()
 				end
 				fileWrite(file,str:sub(3))
 				fileClose(file)
-				outputDebugString("[DGS]Config Updated!")
+				outputDGSMessage("The config file has been updated.")
 			end
 		else
-			outputDebugString("[DGS]Failed to open config file!",3)
+			outputDGSMessage("Failed to open the config file.",3)
 		end
 	else
 		local file = fileCreate("config.txt")
@@ -62,14 +84,14 @@ function loadConfig()
 		end
 		fileWrite(file,str:sub(3))
 		fileClose(file)
-		outputDebugString("[DGS]Config Created!")
+		outputDGSMessage("Config file was created.")
 	end
 
 	setElementData(resourceRoot,"DGS-allowCMD",DGSConfig.enableBuiltInCMD)
 	setElementData(resourceRoot,"DGS-enableDebug",DGSConfig.enableDebug)
 	setElementData(resourceRoot,"DGS-enableCompatibilityCheck",DGSConfig.enableCompatibilityCheck)
 	if DGSConfig.enableG2DCMD then
-		outputDebugString("[DGS]G2D command line is enabled!")
+		outputDGSMessage("G2D command line is enabled.")
 	end
 end
 loadConfig()
@@ -124,6 +146,6 @@ addEvent("DGSI_AbnormalDetected",true)
 addEventHandler("DGSI_AbnormalDetected",root,function(fData)
 	local pName = getPlayerName(client)
 	for fName,fData in pairs(fData) do
-		outputDebugString("[DGS-Security]Abnormal Detected at '"..fName.."' of player '"..pName.."'")
+		outputDGSMessage("Abnormal Detected at '"..fName.."' of player '"..pName.."'",nil,"Security")
 	end
 end)

commit d0a7b95c5dc893028fd203752b0ec768ba6ecfe5

Fix checkServerVersion function
It was checking the opposite. This was caused by https://github.com/thisdp/dgs/commit/1c8b1eae928b42b5e0830e121af826325888229d

update.lua
@@ -52,7 +52,7 @@ function checkUpdate()
 end
 
 function checkServerVersion(player)
-	if getVersion().sortable > "1.5.4-9.11342" then
+	if getVersion().sortable < "1.5.4-9.11342" then
 		outputDGSMessage("Your server version is too old to support dgs update system.",nil,2,player)
 		return false
 	end