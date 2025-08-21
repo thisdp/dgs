54ade2327dbb192a96bcf11863998b924364c889
thisdp <30386052+thisdp@users.noreply.github.com>
2025-08-21 13:01:06 +0800

    fix dgsRoundRectSetTexture

--- plugin/BasicShape/roundedRectangle.lua
+++ plugin/BasicShape/roundedRectangle.lua
@@ -45,7 +45,7 @@ end
 function dgsRoundRectSetTexture(rectShader,texture)
 	if not(dgsGetPluginType(rectShader) == "dgs-dxroundrectangle") then error(dgsGenAsrt(rectShader,"dgsRoundRectSetTexture",1,"plugin dgs-dxroundrectangle")) end
 	if isElement(texture) then
-		if not(isMaterial(texture) == "texture") then error(dgsGenAsrt(dgsCircleSetTexture,"dgsRoundRectSetTexture",1,"texture")) end
+		if not(isMaterial(texture) == "texture") then error(dgsGenAsrt(texture,"dgsRoundRectSetTexture",1,"texture")) end
 		dxSetShaderValue(rectShader,"sourceTexture",texture)
 		dgsSetData(rectShader,"sourceTexture",texture)
 	else
